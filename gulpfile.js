e6ea63fd2959edce2dd897eefe619e9b73c9d713
Slevin Zhang <4ce111@gmail.com>
2016-04-11 15:46:19 -0400

    Ignore files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,20 @@
 // Step 1: Always require gulp
-
+var gulp = require('gulp');
 // Step 3: require more plugins
+var autoprefixer = require('autoprefixer'); 
 
 // Step 2: write tasks
+gulp.task('first', function() {
+  console.log('my first task');
+});
+
+gulp.task('random', function() {
+  var luckyNum = Math.random();
+  console.log(luckyNum);
+});
+
+gulp.task('prefix', function() {
+  return gulp.src('./css/style.css')
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./dist'));
+});
